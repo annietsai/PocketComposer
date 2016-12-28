70e2bf6f5c227dbd2c1853332d0c0ddee53e1f06
Annie Tsai <annietsai@berkeley.edu>
2016-12-27 16:22:44 -0800

    deleted run() contents in Main

--- Main.java
+++ Main.java
@@ -12,17 +12,7 @@ public class Main {
 
     /** Set up and run Pocket Composer. */
     void run() {
-        Game game = new Game();
-        Player player;
-
-        player = null;
-        if (_options.contains("--text")) {
-            player = new TextPlayer(game);
-        } else {
-            player = new GUIPlayer(game);
-        }
-
-        player.play();
+        
     }
 
 }
